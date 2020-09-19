bf2ff11ab266a2b0b8ffda6df8ee135e0e6406e5
J. Hathaway <hathawayj@gmail.com>
2020-09-19 15:42:23 -0600

    sorry didn't export these functions

--- R/dwv_one_student.R
+++ R/dwv_one_student.R
@@ -1,10 +1,18 @@
-
+#' @title Function to create issues
+#' @param one_stdent is a tibble with all the information for one person
+#' @param body_lines is the MarkDown file with a predefined format
+#' @export
 dwv_one_student_issues <-  function(one_student, body_lines) {
   dwv_issue_cs(body_lines, dat = one_student)
   dwv_issue_tasks(body_lines, dat = one_student)
   dwv_issue_semester(body_lines, dat = one_student)
 }
-
+#' @title Function to create repo and connect them to group plus push issues
+#' @param one_stdent is a tibble with all the information for one person
+#' @param body_lines is the MarkDown file with a predefined format
+#' @param ta_group the name of the TA group for that semester.
+#' @param class_group the name of the class group for that semester.
+#' @export
 dwv_one_student <- function(one_student, class_group, ta_group, body_lines = NULL){
   dwv_check_names(one_student)
   dwv_check_repos(one_student)
